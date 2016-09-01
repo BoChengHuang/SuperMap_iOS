f34914e56b1799186804bb63ba55d88ff98e02ef
CNSLab-mini <wabesasa@gmail.com>
2016-09-01 11:47:36 +0800

    update to Swift 3 and GoogleMaps

--- Podfile
+++ Podfile
@@ -1,9 +1,7 @@
-source 'https://github.com/CocoaPods/Specs.git'
-platform :ios, '8.0'
 use_frameworks!
-
-pod 'Socket.IO-Client-Swift', '~> 4.1.1' # Or latest version
-
 source 'https://github.com/CocoaPods/Specs.git'
-platform :ios, '8.1'
-pod 'GoogleMaps'
+target 'SuperMap_iOS_Controller' do
+  pod 'GoogleMaps'
+  pod 'GooglePlaces'
+  pod 'Socket.IO-Client-Swift'
+end
---
+++ SuperMap_iOS_ControllerTests/SuperMap_iOS_ControllerTests.swift
@@ -0,0 +1,36 @@
+//
+//  SuperMap_iOS_ControllerTests.swift
+//  SuperMap_iOS_ControllerTests
+//
+//  Created by CNSLab-mini on 2016/9/1.
+//  Copyright © 2016年 bochengw.twbbs.org. All rights reserved.
+//
+
+import XCTest
+@testable import SuperMap_iOS_Controller
+
+class SuperMap_iOS_ControllerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
